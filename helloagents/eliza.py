2fad08e1c716c92a016c1c20ddf7f3cedeb8f3a4
unknown <zhangfeyy@outlook.com>
2026-02-04 16:45:18 +0100

    MODIFY: add memory for Eliza, but now it looks dumm

--- helloagents/eliza.py
+++ helloagents/eliza.py
@@ -4,7 +4,7 @@ import random
 # 定义规则库，rules是一个字典，键是正则表达式，值是列表
 rules = {
     r'I need (.*)': [
-        "Why do you need {0}?",
+        "Why do you need {0}? I know you are already {age} years old",
         "Would it really help you to get {0}?",
         "Are you sure need {0}?"
     ],
@@ -21,7 +21,7 @@ rules = {
     r'I am (.*)': [
         "Do you come to me because you are {0}?",
         "How long have you been {0}?",
-        "How do you feel about being {0}?"
+        "How do you feel about being {0}, {name}?"
     ],
     r'.* mother .*': [
         "Tell me more about your mother.",
@@ -31,7 +31,12 @@ rules = {
     r'.* father .*': [
         "Tell me more about your father.",
         "How did your father make you feel?",
-        "What has your father taught you?"
+        "What has your father taught you, {name}?"
+    ],
+    r'.* work .*': [
+        "How do you feel about your work? As a {job}",
+        "Do you like your work?",
+        "Can you tell me about your work?"
     ],
     r'.*': [
         "Please tell me more.",
@@ -59,16 +64,41 @@ def respond(user_input):
     """
     根据规则库生成响应
     """
+    for attribute, templates in attributes.items():
+        for template in templates:
+            match = re.search(template, user_input, re.IGNORECASE)
+            if match:
+                info = match.group(1) if match.groups() else ' '
+                user_profile[attribute] = info
+                print("ok, so I know you more now")
+
     for pattern, responses in rules.items():
         match = re.search(pattern, user_input, re.IGNORECASE)
         if match:
             captured_group = match.group(1) if match.groups() else ' '
             swapped_group = swap_pronouns(captured_group)
-            response = random.choice(responses).format(swapped_group) # .format: fill in the blank
-            return response
+            response = random.choice(responses)
+            try:
+                return response.format(swapped_group, **user_profile)
+            except (KeyError, IndexError):
+                return response.format(swapped_group)
 
-    return random.choice(rules[r'.*']) # 字典索引
+    return random.choice(rules[r'.*'])  # 字典索引
 
+
+# 自定义记忆库
+user_profile = {"name": "friend", "age": "30", "job": "student"}
+
+attributes = {
+    "name": [r'i am called (.*)',
+             r'my name is (.*)',
+             r'you can call me (.*)'],
+    "age": [r'i am (.*) years old',
+            r'my age is (.*)'],
+    "job": [r'i am a (.*)',
+            r'i work as a (.*)',
+            r'i am doing a (.*)']
+}
 
 if __name__ == '__main__':
     print("Therapist: Hello! How can I help you today?")
